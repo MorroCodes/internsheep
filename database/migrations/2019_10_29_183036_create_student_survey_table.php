4a222972c7693038f780b00244e713af5ef86eb1
Annelies B <annelies_bellon@hotmail.com>
2019-10-29 19:47:58 +0100

    add student survey migration

---
+++ database/migrations/2019_10_29_183036_create_student_survey_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentSurveyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_surveys', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('vibe');
+            $table->integer('size');
+            $table->integer('age');
+            $table->integer('type');
+            $table->integer('distance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_surveys');
+    }
+}
