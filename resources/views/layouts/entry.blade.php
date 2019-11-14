0a839f1d492f4ec4e8d560e29014b8faec20d193
Annelies B <annelies_bellon@hotmail.com>
2019-11-14 15:16:33 +0100

    add style to login

--- resources/views/layouts/entry.blade.php
+++ resources/views/layouts/entry.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
+<body class="entry-page">
 
 @yield('content')
     
