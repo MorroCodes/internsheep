acce30830897ba3af06118b4c6bbd02fc8fcef79
AqsaIntizar <aqsatje@live.be>
2019-11-14 14:54:49 +0100

    added img folder

--- resources/views/yourCompany.blade.php
+++ resources/views/yourCompany.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts/company')
+@section('nav')
 <ul class="nav">
   <li class="nav-item">
     <a class="nav-link active" href="#">Home</a>
@@ -13,3 +14,15 @@
     <a class="nav-link" href="#">Profiel</a>
   </li>
 </ul>
+@endsection
+
+@section('header')
+<div class="card bg-dark text-white">
+  <img src="../img/bg" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
+@endsection
