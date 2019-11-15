cfdb4cdc66849be1ebe3dfb8c1830100683bdef1
Dielan <dielano@hotmail.be>
2019-11-15 15:25:31 +0100

    can change password

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -23,6 +23,14 @@ class AccountController extends Controller
     }
 
     public function handleNewPassword(Request $request){
-        
+        $password1 = $request->input('password1');
+        $password2 = $request->input('password2');
+        $id = \Auth::user()->id;
+
+        if($password1 === $password2){
+            echo 'hi';
+            $user = \App\User::where('id', $id)->update(['password' => \Hash::make($request->input('password1'))]);
+            return redirect('/youraccount');
+        }
     }
 }
