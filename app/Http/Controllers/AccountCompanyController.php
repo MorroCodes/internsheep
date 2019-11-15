1ed179262f68bbdf6307c875a0a08c9acc4db896
AqsaIntizar <aqsatje@live.be>
2019-11-15 15:37:19 +0100

    change password

--- app/Http/Controllers/AccountCompanyController.php
+++ app/Http/Controllers/AccountCompanyController.php
@@ -19,8 +19,16 @@ class AccountCompanyController extends Controller
 
         return redirect('/companyAccount');
     }
-    public function handleNewPassword(){
+    public function handleNewPassword(Request $request){
+        $password1 = $request->input('password1');
+        $password2 = $request->input('password2');
+        $id = \Auth::user()->id;
 
+        if($password1 === $password2){
+            echo 'hi';
+            $user = \App\User::where('id', $id)->update(['password' => \Hash::make($request->input('password1'))]);
+            return redirect('/youraccount');
+        }
     }
    
 
