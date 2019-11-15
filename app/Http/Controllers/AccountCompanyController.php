90f3b70ed5d0e6febd52f027cdd67d4966c6da33
AqsaIntizar <aqsatje@live.be>
2019-11-15 15:21:35 +0100

    function show added to display view

--- app/Http/Controllers/AccountCompanyController.php
+++ app/Http/Controllers/AccountCompanyController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class AccountCompanyController extends Controller
 {
-    //
+    public function show()
+    {
+        return view('companyAccount');
+    }
+
+   
+
 }
