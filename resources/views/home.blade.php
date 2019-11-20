56f3b59fc3e27eeed9e2f0bfcfb0d9697352bbe4
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-11-20 01:05:08 +0100

    added styling to internships on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,17 @@
 
 @section('content')
 <div class="container">
-    @foreach($internships as $i)
-        <div class="internship_container">
-            <a href="/internship/{{ $i->id }}">
-                <img src="{{ $i->img }}">
-                <h2>{{ $i->title }}</h2>
-            </a>
-        </div>
-    @endforeach
+    <h1>Vind hier een stage die op jouw lijf geschreven staat!</h1>
+    <div class="internships">
+        @foreach($internships as $i)
+            <div class="internship_container">
+                    <img src="{{ $i->img }}">
+                    <div class="content">
+                        <h3>{{ $i->title }}</h3>
+                        <a href="/internship/{{ $i->id }}">Meer info</a>
+                    </div>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
