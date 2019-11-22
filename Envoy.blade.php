6f01e7e6f2729b84220bda5918f8b3dcd8352813
Annelies B <annelies_bellon@hotmail.com>
2019-11-22 14:54:55 +0100

    add envoy file for production website

---
+++ Envoy.blade.php
@@ -0,0 +1,20 @@
+@servers(['production' => 'spammelies@139.162.160.45'])
+
+@task('deploy-production', ['on' =>'production'])
+cd /home/internsheep/internsheep
+    # navigatie juiste map
+php artisan down
+    # website even offline halen (offline scherm)
+git reset --hard HEAD
+    # wijzigingen even opzij zetten (permissie wijzigingen, ...)
+git pull origin master
+composer install
+php composer.phar dump-autoload
+    # dependencies updaten
+php artisan migrate --force
+    # databank updaten
+php artisan cache:clear
+    # cache clearen
+php artisan up
+    # website terug online plaatsen
+@endtask
