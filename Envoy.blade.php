759b12bcad3697c050613576f8232d800f06e073
Annelies B <annelies_bellon@hotmail.com>
2019-11-22 15:32:21 +0100

    finish envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,4 +1,4 @@
-@servers(['production' => 'spammelies@139.162.160.45'])
+@servers(['production' => 'spammelies@139.162.160.45', 'staging' =>'spammelies@139.162.160.45'])
 
 @task('deploy-production', ['on' =>'production'])
 cd /home/internsheep/internsheep
@@ -17,4 +17,16 @@ php artisan cache:clear
     # cache clearen
 php artisan up
     # website terug online plaatsen
+@endtask
+
+@task('deploy-staging', ['on' =>'staging'])
+cd /home/internsheepbeta/internsheep
+php artisan down
+git reset --hard HEAD
+git pull origin master
+composer install
+php composer.phar dump-autoload
+php artisan migrate --force
+php artisan cache:clear
+php artisan up
 @endtask
