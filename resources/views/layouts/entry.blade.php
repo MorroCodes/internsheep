ccf5ab589403a994a82baa0f924905973181c2f4
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-11-25 13:42:53 +0100

    Update entry.blade.php

--- resources/views/layouts/entry.blade.php
+++ resources/views/layouts/entry.blade.php
@@ -7,11 +7,12 @@
     <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.responsivevoice.org/responsivevoice.js?key=0GXKzjlA"></script>
     <title>Document</title>
 </head>
 <body class="entry-page">
 
 @yield('content')
-    
+
 </body>
 </html>
