58206d0118ee71a1f63fde7988c136658c973468
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-11-29 16:22:24 +0100

    Added a relationship in Internship to Company

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts/entry')
 
 @section('content')
 <div class="container">
--- resources/views/student/internshipData.blade.php
+++ resources/views/student/internshipData.blade.php
@@ -1,8 +1,9 @@
-@extends('layouts.app')
+@extends('layouts/entry')
 
 @section('content')
 <div class="container">
     <h3>{{$internship->title}}</h3>
+    <h6>{{$internship->company['company_name']}}</h6>
     <h5>{{$internship->catch_phrase}}</h5>
     <p>{{$internship->description}}</p>
 </div>
