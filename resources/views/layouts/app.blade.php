efd42eec792ee720908dd08bc8f5e36ae1d76545
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-12-01 11:37:06 +0100

    Added link to own student public account

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('changeStudentData') }}">Account</a>
+                                    <a class="dropdown-item" href="{{ route('StudentProfile') }}">Account</a>
+                                    <a class="dropdown-item" href="{{ route('changeStudentData') }}">Settings</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
