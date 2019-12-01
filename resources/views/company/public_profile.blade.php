002fb8211ea019ea36ff42a389041293b9d98a9a
Annelies B <annelies_bellon@hotmail.com>
2019-12-01 16:17:22 +0100

    make public company profile responsive

--- resources/views/company/public_profile.blade.php
+++ resources/views/company/public_profile.blade.php
@@ -14,65 +14,84 @@
         <p>{{$companyInfo->company_bio}}</p>
     </div>
 
-    <div class="company-profile-survey-container">
-        <h2>Leer ons kennen!</h2>
+    <div class="company-profile-content-flex">
+        <div class="company-profile-survey-container">
+            <h2>Leer ons kennen!</h2>
 
-        <div>
-            <p>Werksfeer (informeel-formeel)</p>
-            <div class="survey-result-container">
-                <div class="vibe-check survey-slider" data-score="{{$surveyInfo->vibe}}"></div>
-            </div>
-            <p>{{$surveyInfo->vibe}}</p>
-        </div>
-          
-        <div>
-            <p>Bedrijfsgrootte (Klein KMO - Groot Multinational)</p>
-            <div class="survey-result-container">
-                <div class="size-check survey-slider" data-score="{{$surveyInfo->size}}"></div>
-            </div>
-            <p>{{$surveyInfo->size}}</p>
-        </div>
-        
-        <div>
-            <p>Omschrijving (Jong - Established)</p>
-            <div class="survey-result-container">
-                <div class="age-check survey-slider" data-score="{{$surveyInfo->age}}"></div>
-            </div>
-            <p>{{$surveyInfo->age}}</p>
-        </div>
-
-        <div>
-            <p>Soort stages in aanbieding (Kijkstage - hands-on)</p>
-            <div class="survey-result-container">
-                <div class="type-check survey-slider" data-score="{{$surveyInfo->type}}"></div>
-            </div>
-            <p>{{$surveyInfo->type}}</p>
-        </div>
-
-        <div>
-            <p>Bereikbaaarheid openbaar vervoer (Makkelijk - moeilijker)</p>
-            <div class="survey-result-container">
-                <div class="transport-check survey-slider" data-score="{{$surveyInfo->transport}}"></div>
-            </div>
-            <p>{{$surveyInfo->transport}}</p>
-        </div>
-    </div>
-
-    <div class="company-profile-internships-container">
-        <h2>Vacatures</h2>
-        @foreach($vacatures as $v)
-        <a href="/internship/{{ $v->id }}">
-                <div class="company-internships-card">
-                    <div>
-                        <img src="/{{ $v->img }}" class="card-img-side">
+            <div>
+                <p>Werksfeer</p>
+                <div class="survey-result-container-flex">
+                    <div>informeel</div>
+                    <div class="survey-result-container">
+                        <div class="vibe-check survey-slider" data-score="{{$surveyInfo->vibe}}"></div>
                     </div>
-                    <div class="company-internship-description">
-                            <h3>{{ $v->title }}</h3>
-                        <p>{{ substr($v->functie_omschrijving, 0, 95) . '...' }}</p>
-                    </div>
+                    <div>formeel</div>
                 </div>
-            </a>
-        @endforeach
+            </div>
+            
+            <div>
+                <p>Bedrijfsgrootte</p>
+                <div class="survey-result-container-flex">
+                    <div>Kleine KMO</div>
+                    <div class="survey-result-container">
+                        <div class="size-check survey-slider" data-score="{{$surveyInfo->size}}"></div>
+                    </div>
+                    <div>Grote Multinational</div>
+                </div>
+            </div>
+        
+            
+            <div>
+                <p>Omschrijving</p>
+                <div class="survey-result-container-flex">
+                    <div>Jong</div>
+                    <div class="survey-result-container">
+                        <div class="age-check survey-slider" data-score="{{$surveyInfo->age}}"></div>
+                    </div>
+                    <div>Established</div>
+                </div>
+            </div>
+
+            <div>
+                <p>Soort stages in aanbieding</p>
+                <div class="survey-result-container-flex">
+                    <div>Kijkstage</div>
+                    <div class="survey-result-container">
+                        <div class="type-check survey-slider" data-score="{{$surveyInfo->type}}"></div>
+                    </div>
+                    <div>hands-on</div>
+                </div>
+            </div>
+
+            <div>
+                <p>Bereikbaaarheid openbaar vervoer</p>
+                <div class="survey-result-container-flex">
+                    <div>Makkelijk</div>
+                    <div class="survey-result-container">
+                        <div class="transport-check survey-slider" data-score="{{$surveyInfo->transport}}"></div>
+                    </div>
+                    <div>moeilijker</div>
+                </div>
+            </div>
+        </div>
+    
+        <div class="company-profile-internships-container">
+            <h2>Vacatures</h2>
+            @foreach($vacatures as $v)
+            <a href="/internship/{{ $v->id }}">
+                    <div class="company-internships-card">
+                        <div>
+                            <img src="/{{ $v->img }}" class="card-img-side">
+                        </div>
+                        <div class="company-internship-description">
+                                <h3>{{ $v->title }}</h3>
+                            <p>{{ substr($v->functie_omschrijving, 0, 95) . '...' }}</p>
+                        </div>
+                    </div>
+                </a>
+            @endforeach
+       
+        </div>
     </div>
 </div>
 @endsection
