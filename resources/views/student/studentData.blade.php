472d69dd26c89ef0655ae39b753875b5d5fe4412
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-12-02 09:25:52 +0100

    Require every field in studentData

--- resources/views/student/studentData.blade.php
+++ resources/views/student/studentData.blade.php
@@ -17,20 +17,20 @@
                         <h2>Wijzig gegevens</h2>
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email adres</label>
-                            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{Auth::user()->email}}">
+                            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{Auth::user()->email}}" required>
                         </div>
                         <div class="form-row">
                             <div class="col">
                                 <label for="firstname">Voornaam</label>
-                                <input type="text" name="firstname" class="form-control" id="firstname" value="{{Auth::user()->firstname}}">
+                                <input type="text" name="firstname" class="form-control" id="firstname" value="{{Auth::user()->firstname}}" required>
                             </div>
                             <div class="col">
                                 <label for="lastname">Achternaam</label>
-                                <input type="text" name="lastname" class="form-control" id="lastname" value="{{Auth::user()->lastname}}">
+                                <input type="text" name="lastname" class="form-control" id="lastname" value="{{Auth::user()->lastname}}" required>
                             </div>
                             <div class="form-group">
                                 <label for="bio">Bio</label>
-                                <textarea name="bio" class="form-control" id="bio">{{Auth::user()->description}}</textarea>
+                                <textarea name="bio" class="form-control" id="bio" required>{{Auth::user()->description}}</textarea>
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary">Aanpassen</button>
@@ -53,11 +53,11 @@
                         <h2>Nieuw wachtwoord instellen</h2>
                         <div class="form-group">
                             <label for="inputPassword1">Nieuw wachtwoord</label>
-                            <input type="password" name="password1" class="form-control" id="inputPassword1" placeholder="Password">
+                            <input type="password" name="password1" class="form-control" id="inputPassword1" placeholder="Password" required>
                         </div>
                         <div class="form-group">
                             <label for="inputPassword2">Herhaal nieuw wachtwoord</label>
-                            <input type="password"  name="password2" class="form-control" id="inputPassword2" placeholder="Password">
+                            <input type="password"  name="password2" class="form-control" id="inputPassword2" placeholder="Password" required>
                         </div>
                         <button type="submit" class="btn btn-primary">Aanpassen</button>
                         {{csrf_field()}}
