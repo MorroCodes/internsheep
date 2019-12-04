42ae4df5b711d40c899138f1998aa69eab00e12a
AqsaIntizar <aqsatje@live.be>
2019-12-04 19:58:28 +0100

    layout registratie

--- resources/views/entry/company_signup.blade.php
+++ resources/views/entry/company_signup.blade.php
@@ -1,61 +1,62 @@
-@extends('layouts/entry')
+@extends('layouts.main')
+
 @section('content')
-    <div class="entry-content entry-content-signup">
-        <div class=logo-entry-container>
-            <img src="/images/logo.svg" alt="Internsheep logo" class="logo logo-entry">
+<<div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+       
+          <div class="col-md-12 col-lg-8 mb-5">
+          
+            
+          
+            <form action="" class="p-5 bg-white">
+
+              <div class="row form-group">
+                <div class="col-md-12 mb-3 mb-md-0">
+                  <label class="font-weight-bold" for="firstename">Voornaam</label>
+                  <input type="text" name="firstname" class="form-control" placeholder="Voornaam">
+                </div>
+              </div>
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="font-weight-bold" for="email">Achternaam</label>
+                  <input type="text" name="lastname" class="form-control" placeholder="Achternaam">
+                </div>
+              </div>
+
+
+              <div class="row form-group">
+                <div class="col-md-12 mb-3 mb-md-0">
+                  <label class="font-weight-bold" for="phone">Email</label>
+                  <input type="email" name="email" class="form-control" placeholder="Email">
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="font-weight-bold" for="message">Bedrijfs Naam</label> 
+                  <input type="text" name="company_name" class="form-control" placeholder="Email">
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="font-weight-bold" for="message">Bedrijfsomschrijving</label> 
+                  <textarea name="company_bio" cols="30" rows="5" class="form-control" placeholder="Korte bedrijfsomschrijving"></textarea>
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <input type="submit" value="Send Message" class="btn btn-primary pill px-4 py-2">
+                </div>
+              </div>
+
+  
+            </form>
+          </div>
+          </div>
         </div>
-        <h2>Registreer als bedrijf | <a href="{{ url('/auth/redirect/facebook') }}" class="btn-social">via face<span>bok</span></a></h2>
-        <div class="manual-signup-container">
-        <form action="{{action('EntryController@handleCompanySignup')}}" method="post">
-        @csrf
-
-            @if(!empty($error))
-                <div class="alert alert-danger">{{$error}}</div>
-            @endif
-            <div class="form-group">
-                <label for="firstname">Voornaam</label>
-                <input type="text" name="firstname" class="form-control" value="{{$user['firstname'] ?? ''}}">
-            </div>
-
-            <div class="form-group">
-                <label for="lastname">Achternaam</label>
-                <input type="text" name="lastname" class="form-control" value="{{$user['lastname'] ?? ''}}">
-            </div>
-
-            <div class="form-group">
-                <label for="email">E-mail</label>
-                <input type="email" name="email" class="form-control" value="{{$user['email'] ?? ''}}">
-            </div>
-
-            <div class="form-group">
-                <label for="company_name">Bedrijfsnaam</label>
-                <input type="text" name="company_name" class="form-control" value="{{$user['company_name'] ?? ''}}">
-            </div>
-
-            <div class="form-group">
-                <label for="company_bio">Korte bedrijfsomschrijving</label>
-                <input type="text" name="company_bio" class="form-control" value="{{$user['company_bio'] ?? ''}}">
-            </div>
-
-            <div class="form-group">
-                <label for="password">Wachtwoord</label>
-                <input type="password" name="password" class="form-control">
-            </div>
-
-            <div class="form-group">
-                <label for="password_repeat">Herhaal wachtwoord</label>
-                <input type="password"name="password_repeat" class="form-control">
-            </div>
-
-            <a href="/login">Heb je al een profiel? Meld hier aan.</a>
-
-            <div class="form-group form-group-buttons">
-                <a href="/signup" class="btn btn-light">Vorige</a>
-                <input type="submit" value="Registreer als bedrijf" class="btn btn-primary">
-            </div>
-
-            {{csrf_field()}}
-        </form>
-        </div>  
+      </div>
     </div>
 @endsection
