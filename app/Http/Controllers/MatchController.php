c4b112766db2c9c51df7bcf895564c91a5fc4f1b
MorroCodes <46780762+MorroCodes@users.noreply.github.com>
2019-12-06 01:17:16 +0100

    bereken de afstand en geef op leesbare manier door aan view

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class MatchController extends Controller
 {
-    public function matchStudentWithCompanies()
+    private $token = 'pk.eyJ1IjoibW9ycm9jb2RlcyIsImEiOiJjazNyZm16b2EwOW94M2hwczlsNmJ4Nm45In0.-sQ-jrWzHe90UCnAw4ZSLA';
+
+    public function matchStudentWithCompanies(Request $request)
     {
         if (session('id') == null) {
             return view('entry/login');
@@ -27,6 +31,21 @@ class MatchController extends Controller
         $companySurveys = $this->addMatchPercentages($companySurveys, $percentages);
         $companySurveys = $companySurveys->sortByDesc('match');
         $data['companySurveys'] = $companySurveys;
+        $data['request'] = $request->address;
+
+        if ($request->address != null) {
+            $data['request'] = $this->getGeoCode($request->address);
+            if ($request->tranport_method == null) {
+                $request->transport_method = 'driving';
+            }
+            $destination = $this->getGeoCode($request->address);
+            foreach ($companySurveys as $companySurvey) {
+                foreach ($companySurvey->internships as $internship) {
+                    $origin = $this->getGeoCode($internship->address);
+                    $internship->distance = $this->getDistance($origin, $destination, $request->transport_method);
+                }
+            }
+        }
 
         return view('match/index', $data);
     }
@@ -57,19 +76,68 @@ class MatchController extends Controller
     public function showGeoCode()
     {
         $address = 'Gladioolstraat 5';
-        $data = $this->getGeoCode($address);
+        $p1 = $this->getGeoCode($address);
+        $data['p1'] = $p1;
+        $p2 = $this->getGeoCode('Nieuwstraat Geel');
+        $data['p2'] = $p2;
+        $data['distance'] = $this->getDistance($p1, $p2, 'cycling');
 
         return dd($data);
     }
 
     public function getGeoCode($address)
     {
-        $token = 'pk.eyJ1IjoibW9ycm9jb2RlcyIsImEiOiJjazNyZm16b2EwOW94M2hwczlsNmJ4Nm45In0.-sQ-jrWzHe90UCnAw4ZSLA';
         $address = rawurlencode($address);
-        $url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'.$address.'.json?access_token='.$token;
+        $url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'.$address.'.json?access_token='.$this->token;
         $data = file_get_contents($url);
         $json = json_decode($data, true);
+        $coordinates = $json['features'][0]['geometry']['coordinates'][0].','.$json['features'][0]['geometry']['coordinates'][1];
 
-        return $json['features'][0]['geometry']['coordinates'];
+        return $coordinates;
+    }
+
+    public function getDistance($p1, $p2, $transport_method)
+    {
+        $url = 'https://api.mapbox.com/optimized-trips/v1/mapbox/'.$transport_method.'/'.$p1.';'.$p2.'?access_token='.$this->token;
+        $data = file_get_contents($url);
+        $json = json_decode($data, true);
+        if (isset($json['trips'])) {
+            $result['distance'] = $this->metersToKm($json['trips'][0]['distance']);
+            $result['duration'] = $this->minutesToTime($json['trips'][0]['duration']);
+
+            return $result;
+        }
+
+        return null;
+    }
+
+    public function minutesToTime($mins)
+    {
+        $mins = round($mins);
+        if ($mins < 60) {
+            $mins = $mins.' minuten';
+
+            return $mins;
+        }
+        $hrs = $mins / 60;
+        $hrs = round($hrs, 1);
+        $hrs = $hrs.' uur';
+
+        return $hrs;
+    }
+
+    public function metersToKm($meters)
+    {
+        round($meters);
+        if ($meters < 1000) {
+            $meters = $meters.'m';
+
+            return $meters;
+        }
+        $kilometers = $meters / 1000;
+        $kilometers = round($kilometers, 1);
+        $kilometers = $kilometers.'km';
+
+        return $kilometers;
     }
 }
--- resources/views/match/index.blade.php
+++ resources/views/match/index.blade.php
@@ -2,13 +2,37 @@
 
 @section('content')
 <div class="container">
+    <form action="" method="GET">
+        <div class="form-group">
+            <label for="location_filter">Zoek op locatie:</label>
+            <input class="form-control" type="search" name="address" id="location_filter" value="{{$request}}">
+            <label for="tranports_method">Ik ga</label>
+            <select class="form-control" name="transport_method" id="transport_method">
+                <option value="driving">met de auto</option>
+                <option value="cycling">met de fiets</option>
+                <option value="walking">te voet</option>
+            </select>
+            <button type="submit" class="btn btn-primary">Zoek op locatie</button>
+        </div>
+    </form>
     @foreach ($companySurveys as $companySurvey)
+    <div class="card-deck">
         @foreach ($companySurvey->internships as $internship)
-            <h2>{{$internship->title}}</h2>
-            <h5>{{$companySurvey->match}}% overeenkomst</h5>
-            <p>{{$internship->description}}</p>
-            <img src="{{$internship->img}}" alt="{{$internship->title}}" width="300px" height="200px">
+            <div class="card">
+                {{$internship->img}}
+                <img src="{{$internship->img}}" alt="{{$internship->title}}" class="card-img-top">
+                <div class="card-body">
+                    <h2 class="card-title">{{$internship->title}}</h2>
+                    @if ($internship->distance != null)
+                    <h5 class="card-text">{{$internship->distance["distance"]}}</h5>
+                    <h5 class="card-text">{{$internship->distance["duration"]}}</h5>
+                    @endif
+                <h5 class="card-text">{{$companySurvey->match}}% overeenkomst</h5>
+                <p>{{$internship->description}}</p>
+            </div>
+            </div>
         @endforeach
+    </div>
     @endforeach
 </div>
 @endsection
