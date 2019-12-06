a8ade702a406b0f9ccc4a63ffd257b94e65bae52
MorroCodes <46780762+MorroCodes@users.noreply.github.com>
2019-12-06 15:31:34 +0100

    fix blade template bug

--- resources/views/match/index.blade.php
+++ resources/views/match/index.blade.php
@@ -18,18 +18,22 @@
     @foreach ($companySurveys as $companySurvey)
     @if (!empty($companySurvey->internships->first()) && $companySurvey->company != null)
     <h2>{{$companySurvey->company->first()->company_name}}</h2>
-    <div class="d-flex">
+    <div class="d-flex flex-wrap">
         @foreach ($companySurvey->internships as $internship)
-        <div class="card">
-            <img src="{{$companySurvey->user->first()->profile_image}}" alt="{{$internship->title}}" class="card-img-top" width="300px">
-            <div class="card-body">
-                <h2 class="card-title">{{$internship->title}}</h2>
-                @if ($internship->distance != null)
-                <h5 class="card-text">{{$internship->distance["distance"]}}</h5>
-                <h5 class="card-text">{{$internship->distance["duration"]}}</h5>
-                @endif
-                <h5 class="card-text">{{$companySurvey->match}}% overeenkomst</h5>
-                <p>{{$internship->description}}</p>
+        <div class="col-sm-4">
+            <div class="card">
+                <img src="{{$companySurvey->user->first()->profile_image}}" alt="{{$internship->title}}"
+                    class="card-img-top" width="200px">
+                <div class="card-body">
+                    <h2 class="card-title">{{$internship->title}}</h2>
+                    @if ($internship->distance != null)
+                    <h5 class="card-text">{{$internship->distance["distance"]}}</h5>
+                    <h5 class="card-text">{{$internship->distance["duration"]}}</h5>
+                    @endif
+                    <h5 class="card-text">{{$companySurvey->match}}% overeenkomst</h5>
+                    <p>{{ Str::limit($internship->description, 50) }}</p>
+                    <a href="/internship/{{$internship->id}}" class="btn btn-secondary">Bekijk deze vacature</a>
+                </div>
             </div>
         </div>
         @endforeach
