f738e113c4269443384c9a618ca982bfcbb2be54
MorroCodes <46780762+MorroCodes@users.noreply.github.com>
2019-12-06 13:58:27 +0100

    toon profielfotos bij internships en voeg company names toe tussen secties

--- resources/views/match/index.blade.php
+++ resources/views/match/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
 <div class="container">
@@ -16,25 +16,27 @@
         </div>
     </form>
     @foreach ($companySurveys as $companySurvey)
-    @if (!empty($companySurvey->internships) && $companySurvey->company != null)
-        <h2>{{$companySurvey->company->first()->company_name}}</h2>
-    @endif
+    @if (!empty($companySurvey->internships->first()) && $companySurvey->company != null)
+    <h2>{{$companySurvey->company->first()->company_name}}</h2>
     <div class="card-deck">
         @foreach ($companySurvey->internships as $internship)
-            <div class="card">
-                <img src="{{$internship->img}}" alt="{{$internship->title}}" class="card-img-top">
-                <div class="card-body">
-                    <h2 class="card-title">{{$internship->title}}</h2>
-                    @if ($internship->distance != null)
-                    <h5 class="card-text">{{$internship->distance["distance"]}}</h5>
-                    <h5 class="card-text">{{$internship->distance["duration"]}}</h5>
-                    @endif
+        <div class="card">
+            {{$companySurvey->user->first()}}
+            <img src="{{$internship->img}}" alt="{{$internship->title}}" class="card-img-top">
+            <div class="card-body">
+                <h2 class="card-title">{{$internship->title}}</h2>
+                @if ($internship->distance != null)
+                <h5 class="card-text">{{$internship->distance["distance"]}}</h5>
+                <h5 class="card-text">{{$internship->distance["duration"]}}</h5>
+                @endif
                 <h5 class="card-text">{{$companySurvey->match}}% overeenkomst</h5>
                 <p>{{$internship->description}}</p>
             </div>
-            </div>
+        </div>
         @endforeach
     </div>
+    @endif
+
     @endforeach
 </div>
 @endsection
