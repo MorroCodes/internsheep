1c7313bcc0a0c6cfed0e64d988212f72ddfd172c
Annelies B <annelies_bellon@hotmail.com>
2019-12-07 16:24:25 +0100

    add welcome email queue job to social signup

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -6,6 +6,7 @@ use Redirect;
 use Socialite;
 use App\User;
 use Request;
+use App\Jobs\WelcomeEmailJob;
 
 class SocialController extends Controller
 {
@@ -116,6 +117,8 @@ class SocialController extends Controller
          'provider_id' => $getInfo->id,
      ]);
         }
+        // send email
+        dispatch(new WelcomeEmailJob($user));
 
         return $user;
     }
