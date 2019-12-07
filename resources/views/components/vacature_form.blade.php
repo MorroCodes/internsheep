d31316902deeee992117c8b9579fcd07abad9f75
Annelies B <annelies_bellon@hotmail.com>
2019-12-07 12:57:33 +0100

    change input type textarea to actual textareas

--- resources/views/components/vacature_form.blade.php
+++ resources/views/components/vacature_form.blade.php
@@ -1,19 +1,19 @@
 <div class="form-group">
     <div class="col-md-13 mb-3 mb-md-0">
         <h3 class="create-internship-title">Titel</h3>
-        <input type="text" class="form-control" value="{{ $title ?? '' }}" id="title_vacature" name='title'>
+        <input type="text" class="form-control create-internship-input" value="{{ $title ?? '' }}" id="title_vacature" name='title'>
 
         <h3 class="create-internship-title">Plaats</h3>
-        <input type="text" class="form-control" value="{{ $address ?? '' }}" name='address'>
+        <input type="text" class="form-control create-internship-input" value="{{ $address ?? '' }}" name='address'>
 
         <h3 class="create-internship-title space">Teaser</h3>
-        <input type="textarea" class="form-control" value="{{ $description ?? ''  }}" rows="8" cols="50" name='description'>
+        <textarea name='description' cols="30" rows="3" class="form-control create-internship-input" value="{{ $description ?? ''  }}"></textarea>
 
         <h3 class="create-internship-title">Functie</h3>
-        <input type="textarea" class="form-control" value="{{ $functie_omschrijving ?? ''  }}" rows="8" cols="50" name='functie_omschrijving'>
+        <textarea name='functie_omschrijving' cols="30" rows="5" class="form-control create-internship-input" value="{{ $functie_omschrijving ?? ''  }}"></textarea>
 
         <h3 class="create-internship-title">Ons aanbod</h3>
-        <input type="textarea" class="form-control" value="{{ $aanbod ?? '' }}" rows="8" cols="50" name='aanbod'>
+        <textarea name='aanbod' cols="30" rows="5" class="form-control create-internship-input" value="{{ $aanbod ?? '' }}"></textarea>
 
     </div>
 </div>
