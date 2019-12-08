dc0bc4856a1433900f39d835656db0c75fc48dcf
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-12-08 13:54:17 +0100

    added star rating to internship

--- resources/views/student/internshipData.blade.php
+++ resources/views/student/internshipData.blade.php
@@ -2,35 +2,49 @@
 
 @section('content')
 <div class="site-section bg-light">
-      <div class="container">
+    <div class="container">
         <div class="row">
-          <div class="col-md-12 col-lg-8 mb-5">
-          <div class="container">
-    <h3>{{$internship->title}}</h3>
-    <h5>{{$internship->catch_phrase}}</h5>
-    <p>{{$internship->description}}</p>
-</div>
+            <div class="col-md-12 col-lg-8 mb-5">
+                <div class="container">
+                    <div class="rate">
+                        <input type="radio" id="star5" name="rate" value="5" />
+                        <label for="star5" title="text">5 stars</label>
+                        <input type="radio" id="star4" name="rate" value="4" />
+                        <label for="star4" title="text">4 stars</label>
+                        <input type="radio" id="star3" name="rate" value="3" />
+                        <label for="star3" title="text">3 stars</label>
+                        <input type="radio" id="star2" name="rate" value="2" />
+                        <label for="star2" title="text">2 stars</label>
+                        <input type="radio" id="star1" name="rate" value="1" />
+                        <label for="star1" title="text">1 star</label>
+                    </div>
+                    <h3>{{$internship->title}}</h3>
+                    <h5>{{$internship->catch_phrase}}</h5>
+                    <p>{{$internship->description}}</p>
+                </div>
 
-    <form action="{{ action('AccountController@ApplyInternship') }}" method="post">
+                <form action="{{ action('AccountController@ApplyInternship') }}" method="post">
 
-        <input type="hidden" name="company" value="{{$internship->company_id}}">
-        <input type="hidden" name="internship" value="{{$internship->id}}">
-        <div class="form-group">
-            @if(!empty($error))
-                @component('components/alert')
-                @slot('message') {{$error}} @endslot
-                @slot('alert_type') alert-primary @endslot
-                @endcomponent
-            @endif
-            <label for="reason">Waarom wil je hier stage doen?</label>
-            <textarea name="reason" class="form-control" id="reason"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Solliciteren</button>
-        {{csrf_field()}}
+                    <input type="hidden" name="company" value="{{$internship->company_id}}">
+                    <input type="hidden" name="internship" value="{{$internship->id}}">
+                    <div class="form-group">
+                        @if(!empty($error))
+                        @component('components/alert')
+                        @slot('message') {{$error}}
+                        @endslot
+                        @slot('alert_type') alert-primary
+                        @endslot
+                        @endcomponent
+                        @endif
+                        <label for="reason">Waarom wil je hier stage doen?</label>
+                        <textarea name="reason" class="form-control" id="reason"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Solliciteren</button>
+                    {{csrf_field()}}
 
-    </form>
+                </form>
 
 
 
 
-@endsection
+                @endsection
