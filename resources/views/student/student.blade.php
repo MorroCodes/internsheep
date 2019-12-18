e74ee45272c2fc398c9c8d36072813200c2594e4
AqsaIntizar <aqsatje@live.be>
2019-12-18 17:16:19 +0100

    style added to profile

--- resources/views/student/student.blade.php
+++ resources/views/student/student.blade.php
@@ -1,9 +1,28 @@
-@extends('layouts/main')
+@extends('layouts.main')
 
 @section('content')
-<div class="container profile">
-    <img src="{{ $user->profile_image }}">
-    <h3>{{ $user->firstname }} {{ $user->lastname }}</h3>
-    <p>{{ $user->description }}</p>
-</div>
+
+<div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+       
+          <div class="col-md-12 col-lg-8 mb-5">
+            <div class="p-5 bg-white">
+
+            <img src="{{ $user->profile_image }}">
+              <h5 class="main-title main-title-big">{{$user->firstname}} {{$user->lastname}}</h5>
+              <h5 class="main-title">Bio</h5>
+              <p>{{$user->description}}</p>
+
+              <h5 class="main-title">Contact</h5>
+              <p>{{$user->email}}</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
 @endsection
--- resources/views/student/studentPublic.blade.php
+++ resources/views/student/studentPublic.blade.php
@@ -1,10 +1,34 @@
-@extends('layouts/main')
+@extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <img src="{{ $user->profile_image }}">
-    <h3>{{ $user->firstname }} {{ $user->lastname }}</h3>
-    <p>{{ $user->description }}</p>
-</div>
 
+<div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+       
+          <div class="col-md-12 col-lg-8 mb-5">
+            <div class="p-5 bg-white">
+
+           
+              <h5 class="main-title main-title-big">$user->firstname</h5>
+              <h5 class="main-title">Adres</h5>
+
+              <h5 class="main-title">Beschrijving</h5>
+
+
+              <h5 class="main-title">Functie omschrijving</h5>
+
+              <h5 class="main-title">Aanbod</h5>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-4">
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
 @endsection
