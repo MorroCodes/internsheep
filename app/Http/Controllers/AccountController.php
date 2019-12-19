37b78236aa7ade05a6e1f5cd93ad518eabd25fd2
Annelies B <annelies_bellon@hotmail.com>
2019-12-19 11:58:54 +0100

    change directory

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -157,6 +157,7 @@ class AccountController extends Controller
             echo 'no file!';
         }
     }
+
     public function handleProfilePicture2(Request $request)
     {
         if ($request->hasFile('profile')) {
@@ -232,8 +233,8 @@ class AccountController extends Controller
         for ($i = 0; $i < $charactersLength; ++$i) {
             $randomString .= $characters[rand(0, $charactersLength - 1)];
         }
-        $newDirectory = 'uploads'.DIRECTORY_SEPARATOR.$randomString;
-        mkdir($newDirectory, 0777, true);
+        $newDirectory = './public/uploads'.DIRECTORY_SEPARATOR.$randomString;
+        mkdir($newDirectory, 0755, true);
 
         return $newDirectory;
     }
