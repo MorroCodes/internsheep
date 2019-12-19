d57d35495b5da888a32b97ffb3f765395464a591
Annelies B <annelies_bellon@hotmail.com>
2019-12-19 11:16:28 +0100

    add check for auth

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -13,10 +13,13 @@ class MatchController extends Controller
         if ($request->searchFor != null) {
             return $this->searchByQuery($request);
         }
-        $type = \Auth::user()->type;
-        if ($type != 'student') {
-            return redirect('/yourcompany');
+        if (\Auth::user() != null) {
+            $type = \Auth::user()->type;
+            if ($type != 'student') {
+                return redirect('/yourcompany');
+            }
         }
+
         $userSurvey = \App\StudentSurvey::where('user_id', session('id'))->first();
         if (session('id') != null && !empty($userSurvey)) {
             return $this->matchStudentWithCompanies($userSurvey, $request);
